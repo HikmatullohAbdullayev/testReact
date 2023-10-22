83218243a9ea06e2d363e9b46710ca4aca34217c
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-22 07:44:23 +0500

    delete item

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data , setData] = useState([
+    {title: "salom dunyo", id:1},
+    {title: "xayr dunyo", id:2},
+    {title: "nma gap dunyo", id:3}
+  ])
+
+ 
+  
+
+const deleteItem = (id) => {
+  const filterItem = data.filter((event) =>{
+    return event.id !== id
+  })
+  setData(filterItem)
+}
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>text </h1>
+      {data.map((event) =>{
+        
+        return(
+        <>
+          <h1 key={event.id}>{event.title}</h1>
+          <button onClick={() => deleteItem(event.id)} >delete</button></>
+        )
+      }) }
+    </div>
+   
     </>
   )
 }
