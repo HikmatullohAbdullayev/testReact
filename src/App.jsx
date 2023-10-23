47e5c6db6afe860122f74082deeea14062c79ee4
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-10-23 10:02:27 +0500

    useEffect amaliyot

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { PropsComponent } from './props/porops'
 
 function App() {
+  const [user , setUser] = useState([])
+
   
-
-
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.json(''))
+    .then((data) => setUser(data) )
+  }, [])
+  
+  console.log(user);
   return (
     <>
     <div>
       <PropsComponent name="kimujksan"/>
+      {user.map((item) =>{
+        return(
+          <div>
+            <h2>{item.name}</h2>
+            <p>{item.username}</p>
+            <p>{item.email}</p>
+            <p>{item.address.street}</p>
+          </div>
+        )
+      })}
     </div>
    
     </>
